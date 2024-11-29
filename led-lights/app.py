229f9b1f1e34f175825c73eec72c84aea662c473
Marshal Currier <marshalcurrier@gmail.com>
2024-11-29 16:51:44 -0700

    update

--- led-lights/app.py
+++ led-lights/app.py
@@ -15,14 +15,26 @@ def home():
                            app_name = app_name,
                            page_title = page_title)
 
-@app.route('/off', methods=['POST'])
-def off():
+
+
+@app.route('/white/off', methods=['POST'])
+def white_off():
     GPIO.output(18, GPIO.LOW)
     return 'light off'
 
-@app.route('/on', methods=['POST'])
-def on():
+@app.route('/white/on', methods=['POST'])
+def white_on():
     GPIO.output(18, GPIO.HIGH)
+    return 'light on'
+
+@app.route('/red/off', methods=['POST'])
+def red_off():
+    GPIO.output(17, GPIO.LOW)
+    return 'light off'
+
+@app.route('/red/on', methods=['POST'])
+def red_on():
+    GPIO.output(17, GPIO.HIGH)
     return 'light on'
 
 if __name__ == '__main__':
