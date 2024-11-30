a19b0950054bab99c66cb631838d86cd172c7a8d
Marshal Currier <marshalcurrier@gmail.com>
2024-11-29 17:23:18 -0700

    test

--- led-lights/app.py
+++ led-lights/app.py
@@ -41,25 +41,25 @@ def light_status(light, status):
     GPIO.output(18, GPIO.LOW)
     return 'ok'
 
-@app.route('/white/off', methods=['POST'])
-def white_off():
-    GPIO.output(18, GPIO.LOW)
-    return 'light off'
+# @app.route('/white/off', methods=['POST'])
+# def white_off():
+#     GPIO.output(18, GPIO.LOW)
+#     return 'light off'
 
-@app.route('/white/on', methods=['POST'])
-def white_on():
-    GPIO.output(18, GPIO.HIGH)
-    return 'light on'
+# @app.route('/white/on', methods=['POST'])
+# def white_on():
+#     GPIO.output(18, GPIO.HIGH)
+#     return 'light on'
 
-@app.route('/red/off', methods=['POST'])
-def red_off():
-    GPIO.output(17, GPIO.LOW)
-    return 'light off'
+# @app.route('/red/off', methods=['POST'])
+# def red_off():
+#     GPIO.output(17, GPIO.LOW)
+#     return 'light off'
 
-@app.route('/red/on', methods=['POST'])
-def red_on():
-    GPIO.output(17, GPIO.HIGH)
-    return 'light on'
+# @app.route('/red/on', methods=['POST'])
+# def red_on():
+#     GPIO.output(17, GPIO.HIGH)
+#     return 'light on'
 
 if __name__ == '__main__':
     app.run(host='192.168.0.63', port=80, debug=True, threaded=False)
